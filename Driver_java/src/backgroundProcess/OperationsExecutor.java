b00812cb08175bfaf4e64b3d76c9b9c0f9eb6ade
CristinaRiveraV <cristina.rivera.valdez@gmail.com>
2021-08-04 22:24:00 +0100

    Operations Executor
    Finished class and all methods.
    
    All the methods have a small diagram showing the pseudocode.

--- Driver_java/src/backgroundProcess/OperationsExecutor.java
+++ Driver_java/src/backgroundProcess/OperationsExecutor.java
@@ -1,5 +1,7 @@
 package backgroundProcess;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Queue;
 
 /**
@@ -43,8 +45,10 @@ public class OperationsExecutor {
 	 * Infinite loop that breaks when there is no operation in progress
 	 * 
 	 * @return true when system is free to execute a new command
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 * */
-	private boolean checkStatus() {
+	private boolean checkStatus() throws UnknownHostException, IOException {
 		while(true) {
 			if(!RobotConnection.getInstance().get_operation_in_progress()) return true; 
 		}
@@ -58,53 +62,120 @@ public class OperationsExecutor {
 	 * Robot will continue to run the process until it is finished.
 	 * 
 	 * @param operation "pick", "place" or "transfer"
-	 * @param parameterNames[] "SourceLocation" or "DestinationLocation"
+	 * @param parameterNames[] a String array with the following text: "SourceLocation" or "DestinationLocation"
+	 * @param parameterValues[] an Integer array with 1 or 2 values
 	 * 
-	 * @return empty string if the item was added succesfully
+	 * @return empty string if the item was sent successfully
+	 * @return "Another item is in progress"
 	 * */
-	public String execute(String operation, String[]parameterNames, int[]parameterValues) {
-		return "";
+	public String execute(String operation, String[] parameterNames, int[] parameterValues) {
+		
+		switch(operation) {
+			case "pick": try {
+							return this.pick(parameterNames, parameterValues);
+						} catch (UnknownHostException e) {
+							return "Could not find host.";
+						} catch (ParametersDontMatch e) {
+							return e.toString();
+						} catch (IOException e) {
+							return "Internal Error. IO Exception";
+						}
+			case "place": try {
+							return this.place(parameterNames, parameterValues);
+						} catch (UnknownHostException e) {
+							return "Could not find host.";
+						} catch (ParametersDontMatch e) {
+							return e.toString();
+						} catch (IOException e) {
+							return "Internal Error. IO Exception";
+						}
+			case "transfer": try {
+							return this.place(parameterNames, parameterValues);
+						} catch (UnknownHostException e) {
+							return "Could not find host.";
+						} catch (ParametersDontMatch e) {
+							return e.toString();
+						} catch (IOException e) {
+							return "Internal Error. IO Exception";
+						}
+			default: OperationUnknown exception = new OperationUnknown();
+						return exception.toString();
+			}
+		
 	}
 	
 	
 	
-	/**
-	 * Checks ig there is any operation in progress.
-	 * If there is no operation in progress it send the next string message to the robot.
-	 * 
-	 * to be used when the button is clicked. 
-	 * 
-	 * @return String when the operations were successfull
-	 * */
-	private String sendOperationMessage(Operation o) {
-		return RobotConnection.getInstance().sendMessage(o.toString());
-	}
+	
 	
 	/**
-	 * Checks all items are correct before creating a "pick" operation
+	 * Checks all items are correct before creating a "pick" operation and sending it to the Robot 
 	 * 
 	 * @param paramNames = "SourceLocation" //ignore case
 	 * @param paramValues should have length of 1
 	 * 
 	 * @return an empty string when successful
-	 * @exception WrongParamNumber
-	 * @exception WrongParamName 
+	 * @exception ParametersDontMatch
+	 * @throws IOException 
+	 * @throws UnknownHostException 
+	 * @throws Exception 
 	 * */
-	private String pick(String[] paramNames, int[] paramValues) {
+	private String pick(String[] paramNames, int[] paramValues) throws ParametersDontMatch, UnknownHostException, IOException {
+		//
+		if(paramNames.length != 1 || paramValues.length != 1) throw new ParametersDontMatch("pick");
+		if(!(paramNames[0].replaceAll("\\s","")).equalsIgnoreCase("Sourcelocation")) throw new ParametersDontMatch("pick");
+		
+		if(!RobotConnection.getInstance().get_empty_hand()) return "Cannot pickup. Holding something";
+		
+		Operation op = new Operation("pick",paramValues[0]);
+		String command  = RobotConnection.getInstance().sendMessage(op.toString());
+
+		try {
+			process_in_progress_id =Integer.valueOf(command);
+		}catch(NumberFormatException v) {
+			return "Internal error. String could not be formated as integer";
+		}
+		
+				
+		if(process_in_progress_id < 0) { //if the number returned is negative there is an operation in progress
+			return "Another process is in progress";
+		}
+		RobotConnection.getInstance().change_empty_hand();
 		return "";
 	}
 	
 	/**
-	 * Checks all items are correct before creating a "place" operation
+	 * Checks all items are correct before creating a "place" operation  and sending it to the Robot 
 	 * 
 	 * @param paramNames = "DestinationLocation" //ignore case
 	 * @param paramValues should have length of 1
 	 * 
 	 * @return an empty string when successful
-	 * @exception WrongParamNumber
-	 * @exception WrongParamName 
+	 * @exception ParametersDontMatch
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 * */
-	private String place(String[] paramNames, int[] paramValues) {
+	private String place(String[] paramNames, int[] paramValues) throws ParametersDontMatch, UnknownHostException, IOException {
+		//
+		if(paramNames.length != 1 || paramValues.length != 1) throw new ParametersDontMatch("place");
+		if(!(paramNames[0].replaceAll("\\s","")).equalsIgnoreCase("Destinationlocation")) throw new ParametersDontMatch("place");
+		
+		if(RobotConnection.getInstance().get_empty_hand()) return "Cannot place without holding something first";
+		
+		Operation op = new Operation("place",paramValues[0]);
+		String command  = RobotConnection.getInstance().sendMessage(op.toString());
+
+		try {
+			process_in_progress_id =Integer.valueOf(command);
+		}catch(NumberFormatException v) {
+			return "Internal error. String could not be formated as integer";
+		}
+		
+				
+		if(process_in_progress_id < 0) { //if the number returned is negative there is an operation in progress
+			return "Another process is in progress";
+		}
+		RobotConnection.getInstance().change_empty_hand();
 		return "";
 	}
 	/**
@@ -116,26 +187,52 @@ public class OperationsExecutor {
 	 * 
 	 * @return an empty string when successful
 	 * 
-	 * @exception WrongParamNumber
-	 * @exception WrongParamName 
+	 * @exception ParametersDontMatch
+	 * @throws IOException 
+	 * @throws UnknownHostException 
 	 * */
-	private String transfer(String[] paramNames, int[] paramValues) {
+	private String transfer(String[] paramNames, int[] paramValues) throws ParametersDontMatch, UnknownHostException, IOException {
+		if(paramNames.length != 2 || paramValues.length != 2) throw new ParametersDontMatch("place");
+		
+		if(!(paramNames[0].replaceAll("\\s","")).equalsIgnoreCase("Destinationlocation")) throw new ParametersDontMatch("place");
+		
+		
+		if("" .equals(pick(new String[] {paramNames[0]},new int[] {paramValues[0]}))) {
+			process_in_progress_id = RobotConnection.getInstance().waitForProcessToEnd("pick%"+ paramNames[0]);
+			return "";
+			
+		}
+		
+		pick(new String[] {paramNames[1]},new int[] {paramValues[1]});
+		process_in_progress_id = RobotConnection.getInstance().waitForProcessToEnd("place%"+ paramNames[0]);
 		return "";
+		
+		
+		
 	}
 	/** For Testing private method "pick"
+	 * @throws IOException 
+	 * @throws ParametersDontMatch 
+	 * @throws UnknownHostException 
 	 * */
-	public String test_pick(String[] paramNames, int[] paramValues) {
+	public String test_pick(String[] paramNames, int[] paramValues) throws UnknownHostException, ParametersDontMatch, IOException {
 		return pick(paramNames, paramValues);
 	}
 	/** 
 	 * 	For Testing private method "place"	 
+	 * @throws IOException 
+	 * @throws ParametersDontMatch 
+	 * @throws UnknownHostException 
 	 *  */
-	public String test_place(String[] paramNames, int[] paramValues) {
+	public String test_place(String[] paramNames, int[] paramValues) throws UnknownHostException, ParametersDontMatch, IOException {
 		return pick(paramNames, paramValues);
 	}
 	/** 	For Testing private method "transfer"
+	 * @throws IOException 
+	 * @throws ParametersDontMatch 
+	 * @throws UnknownHostException 
 	 * */
-	public String test_transfer(String[] paramNames, int[] paramValues) {
+	public String test_transfer(String[] paramNames, int[] paramValues) throws UnknownHostException, ParametersDontMatch, IOException {
 		return pick(paramNames, paramValues);
 	}
 }
